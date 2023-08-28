97e094b16ee1c79becbaada6e8aa97964e0195be
PeterZabila <petrozabila@gmail.com>
2023-08-28 17:28:26 +0300

    3rd commit

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,18 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface STR {
+  title: string
+}
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<STR> {
   pageInfo () {
     console.log(this.props.title);
   }
 }
-
-export {};
